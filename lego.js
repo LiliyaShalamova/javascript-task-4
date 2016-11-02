5bb092b21e4761cdefa4e45f4301d14fdf33f1e7
Лилия Шаламова < >
2016-11-02 18:13:31 +0500

    Первый

--- lego.js
+++ lego.js
@@ -4,8 +4,15 @@
  * Сделано задание на звездочку
  * Реализованы методы or и and
  */
-exports.isStar = true;
+exports.isStar = false;
 
+var PRIORITIES_OF_FUNCTIONS = {
+    'filterIn': 1,
+    'sortBy': 2,
+    'select': 3,
+    'format': 4,
+    'limit': 5
+};
 /**
  * Запрос к коллекции
  * @param {Array} collection
@@ -13,16 +20,53 @@ exports.isStar = true;
  * @returns {Array}
  */
 exports.query = function (collection) {
-    return collection;
+    var copy = getCopy(collection);
+    if (arguments.length === 1) {
+        return copy;
+    }
+    var functions = [].slice.call(arguments).slice(1);
+    var sortedFunctions = functions.sort(function (func1, func2) {
+        return PRIORITIES_OF_FUNCTIONS[func1.name] > PRIORITIES_OF_FUNCTIONS[func2.name] ? 1 : -1;
+    });
+    for (var func = 0; func < sortedFunctions.length; func++) {
+        copy = sortedFunctions[func](copy);
+    }
+
+    return copy;
 };
 
+function getCopy(collection) {
+    var copy = [];
+    for (var i = 0; i < collection.length; i++) {
+        var person = {};
+        var fields = Object.keys(collection[i]);
+        for (var field in fields) {
+            person[fields[field]] = collection[i][fields[field]];
+        }
+        copy.push(person);
+    }
+    return copy;
+}
 /**
  * Выбор полей
  * @params {...String}
  */
+
 exports.select = function () {
-    return;
-};
+    var args = [].slice.call(arguments);
+    
+    return function select(collection) {
+        for (var person = 0; person < collection.length; person++) {
+            var keys = Object.keys(collection[person]);
+            for (var property = 0; property < keys.length; property++) {
+                if (args.indexOf(keys[property]) === -1) {
+                     delete collection[person][keys[property]];
+                 }
+            }
+        }
+        return collection;
+    }
+  };
 
 /**
  * Фильтрация поля по массиву значений
@@ -30,9 +74,13 @@ exports.select = function () {
  * @param {Array} values – Доступные значения
  */
 exports.filterIn = function (property, values) {
-    console.info(property, values);
+    //console.info(property, values);
 
-    return;
+    return function filterIn(collection) {
+        return collection.filter(function (person) {
+            return (values.indexOf(person[property]) !== -1);
+        });
+    };
 };
 
 /**
@@ -41,9 +89,16 @@ exports.filterIn = function (property, values) {
  * @param {String} order – Порядок сортировки (asc - по возрастанию; desc – по убыванию)
  */
 exports.sortBy = function (property, order) {
-    console.info(property, order);
+    //console.info(property, order);
 
-    return;
+    return function sortBy(collection) {
+        return collection.sort(function (person1, person2) {
+            if (order === 'asc') {
+                return person1[property] > person2[property];
+            }
+            return person2[property] < person1[property];
+        });
+    };
 };
 
 /**
@@ -52,9 +107,17 @@ exports.sortBy = function (property, order) {
  * @param {Function} formatter – Функция для форматирования
  */
 exports.format = function (property, formatter) {
-    console.info(property, formatter);
+    //console.info(property, formatter);
 
-    return;
+    return function format(collection) {
+        return collection.map(function (person) {
+            if (person.hasOwnProperty(property)) {
+                person[property] = formatter(person[property]);
+            }
+
+            return person;
+        });
+    };
 };
 
 /**
@@ -62,10 +125,12 @@ exports.format = function (property, formatter) {
  * @param {Number} count – Максимальное количество элементов
  */
 exports.limit = function (count) {
-    console.info(count);
+    //console.info(count);
 
-    return;
-};
+    return function limit(collection) {
+        return collection.slice(0, count);
+        };
+    };
 
 if (exports.isStar) {
 
